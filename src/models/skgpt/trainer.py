469b750e44f6c0748cd8ede36240f88fcffca964
Samuel Cruz <cruzas@users.noreply.github.com>
2025-01-20 18:08:42 +0100

    some progress: created trainer

--- src/models/skgpt/trainer.py
+++ src/models/skgpt/trainer.py
@@ -9,6 +9,8 @@ from collections import defaultdict
 import torch
 from torch.utils.data.dataloader import DataLoader
 
+from src.optimizers.apts import APTS
+from src.optimizers.trust_region import TrustRegion
 from src.pmw.dataloaders import GeneralizedDistributedDataLoader
 from src.pmw.model_handler import ModelHandler
 from src.utils import CfgNode as CN
@@ -29,10 +31,32 @@ class Trainer:
         C.batch_size = 64
         # the model we're using is so small that we can go a bit faster
         C.learning_rate = 1e-3
-        C.max_subdomain_iters = 3
         C.betas = (0.9, 0.95) # in case of Adam optimizer used somewhere
         C.weight_decay = 0.1  # only applied on matmul weights
         C.grad_norm_clip = 1.0 # max norm for gradient clipping
+        # subdomain optimizer
+        C.subdomain_optimizer = torch.optim.AdamW
+        C.subdomain_optimizer_args = {'lr' : C.learning_rate}
+        if C.subdomain_optimizer == torch.optim.AdamW:
+            C.subdomain_optimizer_args['betas'] = C.betas
+        elif C.subdomain_optimizer == torch.optim.SGD:
+            C.subdomain_optimizer_args['momentum'] = 0.9
+        C.max_subdomain_iters = 3
+        # global optimizer
+        C.global_optimizer = TrustRegion
+        C.global_optimizer_args = {
+            'lr': C.learning_rate,
+            'max_lr': 1.0,
+            'min_lr': 0.0001,
+            'nu': 0.5,
+            'inc_factor': 2.0,
+            'dec_factor': 0.5,
+            'nu_1': 0.25,
+            'nu_2': 0.75,
+            'max_iter': 3,
+            'norm_type': 2
+        }
+
         return C
 
     def __init__(self, config, model, train_dataset):
@@ -70,6 +94,18 @@ class Trainer:
 
         # setup the optimizer
         model.configure_params(config)
+        self.optimizer = APTS(model=self.model,
+                              subdomain_optimizer=config.subdomain_optimizer,
+                              subdomain_optimizer_defaults=config.subdomain_optimizer_args,
+                              global_optimizer=config.global_optimizer,
+                              global_optimizer_defaults=config.global_optimizer_args,
+                              lr=config.learning_rate,
+                              max_subdomain_iter=config.max_subdomain_iters,
+                              dogleg=True,
+                              APTS_in_data_sync_strategy='average', 
+                              step_strategy='mean'
+                              )
+
         train_loader = GeneralizedDistributedDataLoader(model_handler=config.model_handler, 
                                                         dataset=self.train_dataset, 
                                                         batch_size=config.batch_size, 
