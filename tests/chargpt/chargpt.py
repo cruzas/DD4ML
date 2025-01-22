5455c7498bc1c323dbd7055ca12183041e12122f
Samuel Cruz <cruzas@users.noreply.github.com>
2025-01-22 15:02:50 +0100

    Unified some dataset stuff

--- tests/chargpt/chargpt.py
+++ tests/chargpt/chargpt.py
@@ -9,6 +9,7 @@ import torch
 from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
 
+from datasets.char_dataset import CharDataset
 from src.models.gpt.mingpt.model import GPT
 from src.models.gpt.mingpt.trainer import Trainer
 from src.utils import CfgNode as CN
@@ -41,52 +42,6 @@ def get_config():
     return C
 
 # -----------------------------------------------------------------------------
-
-
-class CharDataset(Dataset):
-    """
-    Emits batches of characters
-    """
-
-    @staticmethod
-    def get_default_config():
-        C = CN()
-        C.block_size = 128
-        return C
-
-    def __init__(self, config, data):
-        self.config = config
-
-        chars = sorted(list(set(data)))
-        data_size, vocab_size = len(data), len(chars)
-        print('data has %d characters, %d unique.' % (data_size, vocab_size))
-
-        self.stoi = {ch: i for i, ch in enumerate(chars)}
-        self.itos = {i: ch for i, ch in enumerate(chars)}
-        self.vocab_size = vocab_size
-        self.data = data
-
-    def get_vocab_size(self):
-        return self.vocab_size
-
-    def get_block_size(self):
-        return self.config.block_size
-
-    def __len__(self):
-        return len(self.data) - self.config.block_size
-
-    def __getitem__(self, idx):
-        # grab a chunk of (block_size + 1) characters from the data
-        chunk = self.data[idx:idx + self.config.block_size + 1]
-        # encode every character to an integer
-        dix = [self.stoi[s] for s in chunk]
-        # return as tensors
-        x = torch.tensor(dix[:-1], dtype=torch.long)
-        y = torch.tensor(dix[1:], dtype=torch.long)
-        return x, y
-
-# -----------------------------------------------------------------------------
-
 
 if __name__ == '__main__':
 
