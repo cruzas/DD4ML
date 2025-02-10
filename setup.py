d2f4225126ade43bd13bdc1df18ce73679769851
Samuel Adolfo Cruz Alegria <scruzale@daint-ln003.cscs.ch>
2025-02-10 15:39:51 +0100

    Starting to put files together for execution on daint.alps.

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name="DD4ML",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "torch",
+        "torchvision",
+        "torchaudio",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scipy",
+        "requests",
+        "wandb",
+        "pyyaml",
+    ],
+)
