98498e58765d76cfa4d11659a93f346a27447ad8
Samuel Adolfo Cruz Alegria <scruzale@daint-ln001.cscs.ch>
2025-02-11 17:39:42 +0100

    Modified tests. Removed old history. More secure now.

---
+++ tests/compute_averages.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from src.utils import compute_best_lr_per_batch_size, parse_cmd_args
+
+
+def main():
+    args = parse_cmd_args()
+    verbose = True
+    
+    # Build project reference using command-line arguments.
+    project = f"{args.entity}/{args.project}"
+    compute_best_lr_per_batch_size(project, metric="loss", verbose=verbose)
+    compute_best_lr_per_batch_size(project, metric="accuracy", verbose=verbose)
+    
+
+if __name__ == "__main__":
+    main()
